124c0eb9246a237436d0e72cd6d61e06a8ee80b2
Dan Lew <danlew42@gmail.com>
2015-03-13 16:10:22 -0500

    Added initial vector drawable test

--- app/app/src/main/java/net/danlew/drawableperf/MainActivity.java
+++ app/app/src/main/java/net/danlew/drawableperf/MainActivity.java
@@ -1,37 +1,116 @@
 package net.danlew.drawableperf;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.functions.Func2;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final List<Integer> VECTOR_DRAWABLES = Arrays.asList(
+        R.drawable.ic_3d_rotation_24px_vector,
+        R.drawable.ic_accessibility_24px_vector,
+        R.drawable.ic_account_balance_24px_vector,
+        R.drawable.ic_account_balance_wallet_24px_vector,
+        R.drawable.ic_account_box_24px_vector,
+        R.drawable.ic_account_child_24px_vector,
+        R.drawable.ic_account_circle_24px_vector,
+        R.drawable.ic_add_shopping_cart_24px_vector,
+        R.drawable.ic_alarm_24px_vector,
+        R.drawable.ic_alarm_add_24px_vector,
+        R.drawable.ic_alarm_off_24px_vector,
+        R.drawable.ic_alarm_on_24px_vector
+    );
+
+    @InjectView(R.id.size_edit_text)
+    EditText mSizeEditText;
+
+    @InjectView(R.id.iterations_edit_text)
+    EditText mIterationsEditText;
+
+    @InjectView(R.id.results)
+    TextView mResultsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @OnClick(R.id.vector_drawable_test)
+    public void testVectorDrawable() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            Toast.makeText(this, "VectorDrawables are only supported in Lollipop+", Toast.LENGTH_LONG).show();
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        Observable<Drawable> drawableObservable =
+            Observable.from(VECTOR_DRAWABLES)
+                .map(new Func1<Integer, Drawable>() {
+                    @Override
+                    public Drawable call(Integer resId) {
+                        return getResources().getDrawable(resId);
+                    }
+                });
+
+        testDrawables(drawableObservable);
+    }
+
+    @OnClick(R.id.mr_vector_test)
+    public void testMrVector() {
+        // TODO
+    }
+
+    @OnClick(R.id.bitmap_drawable_test)
+    public void testBitmap() {
+        // TODO
+    }
+
+    private void testDrawables(Observable<Drawable> drawables) {
+        int size = Integer.parseInt(mSizeEditText.getText().toString());
+        int iterations = Integer.parseInt(mIterationsEditText.getText().toString());
+
+        drawables.reduce(new DrawableInstrumentation(iterations, size, size),
+            new Func2<DrawableInstrumentation, Drawable, DrawableInstrumentation>() {
+                @Override
+                public DrawableInstrumentation call(DrawableInstrumentation drawableInstrumentation,
+                                                    Drawable drawable) {
+                    drawableInstrumentation.testDrawable(drawable);
+                    return drawableInstrumentation;
+                }
+            })
+            .subscribe(new Action1<DrawableInstrumentation>() {
+                @Override
+                public void call(DrawableInstrumentation drawableInstrumentation) {
+                    long avg = drawableInstrumentation.getAverageNanosecondsPerRender();
+                    String result = "Average render time per drawable: " + avg + " ns";
+                    result += "\n\nThis works out to " + (avg / 100000) + " ms per 10 drawables rendered";
+                    result += "\n\nOr... " + (avg / 10000) + " ms per 100 drawables rendered";
+                    mResultsTextView.setText(result);
+                }
+            },
+            new Action1<Throwable>() {
+                @Override
+                public void call(Throwable throwable) {
+                    Log.e("DrawablePerf", "Something went wrong during testing", throwable);
+                }
+            });
     }
 }
